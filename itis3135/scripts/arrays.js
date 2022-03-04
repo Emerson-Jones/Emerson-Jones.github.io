c069ffee04c0cb19266fd8146fef5b5644d6077d
Emerson-Jones <72284247+Emerson-Jones@users.noreply.github.com>
2022-03-03 23:06:45 -0500

    arrays

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,21 @@
+var person = {};
+var salary = {};
+
+document.getElementById("addSalary").onclick = addSalary;
+document.getElementById("displayResults").onclick = displayResults;
+document.getElementById("displaySalary").onclick = displaySalary;
+
+function addSalary()
+{
+
+}
+
+function displayResults()
+{
+
+}
+
+function displaySalary()
+{
+
+}
