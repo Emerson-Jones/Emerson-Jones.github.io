50cc3c6989fee16967c324622c4e92d625a12332
Emerson-Jones <72284247+Emerson-Jones@users.noreply.github.com>
2022-03-25 15:41:43 -0400

    slideshow

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n)
+{
+    showslides(slideIndex += n);
+}
+
+function currentSlide(n) 
+{
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++)
+    {
+        slides[i].getElementsByClassName.display = "none"
+    }
+    for (i = 0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex-1].getElementsByClassName.display = "block";
+    dots[slideIndex-1].className += " active";
+}
