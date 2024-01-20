3add8ecb9643a3d8be2fce89d219934bf3157c9c
Emerson-Jones <72284247+Emerson-Jones@users.noreply.github.com>
2024-01-19 22:29:03 -0500

    website redo

--- itis3135/Activity11/Jones-Activity11.js
+++
@@ -1,9 +0,0 @@
-/* jQuery call to the accordion() method. */
-$(document).ready(function() {
-    $("#accordion").accordion({
-        event: "mouseover",
-        heightStyle: "content",
-        collapsible: true
-    });
-
-});
--- project/Scripts/accordion.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#accordion").accordion({
-        event: "mouseover",
-        heightStyle: "content",
-        collapsible: true
-    });
-
-});
